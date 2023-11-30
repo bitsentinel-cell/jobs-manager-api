7882b37290cd937824b5f614e4107f0af25ace88
bitsentinel-cell <mehransentinel#gmail.com>
2023-11-30 15:02:26 +0330

    root url installed

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ app.use(cors());
 app.use(xss())
 
 
-
+app.get('/' , (req,res)=>{
+    return res.send('hello from express job api manager')
+})
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/jobs' , auth , jobRoutes);
