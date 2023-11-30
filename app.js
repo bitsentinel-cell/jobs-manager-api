6d3501134ca5c8d01b69a4158a918995a8c54c44
bitsentinel-cell <mehransentinel#gmail.com>
2023-11-30 13:49:47 +0330

    adding security packages

--- app.js
+++ app.js
@@ -7,10 +7,34 @@ import dotenv from 'dotenv';
 dotenv.config();
 import auth from "./src/middlewares/authentication.js";
 
+
+// security packages
+import helmet from 'helmet';
+import cors from 'cors'
+import xss from 'xss-clean'
+import rateLimit from "express-rate-limit";
+
+
+
+
 const app = express();
 const port = process.env.PORT | 8000;
 
+
+app.set('trust proxy', 1);
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+);
+
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss())
+
+
 
 
 app.use('/api/v1/auth', authRoutes);
